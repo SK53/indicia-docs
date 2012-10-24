7e4a010d43cf5608dc3e712e2073d916287766a1
John van Breda <john.vanbreda@biodiverseit.co.uk>
2012-10-24 18:20:23 +0100

    Default build excludes the alternate docs folder
    This removes some make warnings

--- conf.py
+++ conf.py
@@ -66,7 +66,7 @@ release = '0.8.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build','alternate-docs']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
